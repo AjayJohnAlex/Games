28be961922e8391c0ffbf8a63edea9812c2dd305
AjayJohnAlex <ajay.alex96@gmail.com>
2020-02-16 23:21:10 +0530

    Adding Boundaries and Jumping

--- Assassin/first.py
+++ Assassin/first.py
@@ -15,6 +15,8 @@ vel = 5
 
 screen_width = 500
 
+is_Jump = False
+jumpCount = 10
 # all pygame has a main loop which checks for collition , mouse events , movements etc
 
 run = True
@@ -33,11 +35,27 @@ while run:
     if keys[pygame.K_RIGHT] and x < screen_width - width - vel:
         x += vel
 
-    if keys[pygame.K_UP] and y > vel:
-        y -= vel
+    if not(is_Jump):
 
-    if keys[pygame.K_DOWN] and y < screen_width - height - vel:
-        y += vel
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+
+        if keys[pygame.K_DOWN] and y < screen_width - height - vel:
+            y += vel
+
+        if keys[pygame.K_SPACE]:
+            is_Jump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+
+        else:
+            is_Jump = False
+            jumpCount = 10
 
     # fill the screen before drawing the character
 
@@ -45,7 +63,7 @@ while run:
 
     # draw our character
     # every character in pygame is a surface and it lies on the window and colors all are in RGB
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width,    height))
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
     # to show you need to refresh the window
     pygame.display.update()
 
